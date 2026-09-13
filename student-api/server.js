355a2bc61e6d43f26075524dfd17708a9479a954
Aryan107rn <aryannakade123@gmail.com>
2026-09-13 20:35:42 +0530

    middleware added and tested with postman:

--- student-api/server.js
+++ student-api/server.js
@@ -1,4 +1,18 @@
-const express = require("express");
+import express from "express";
 
 const app = express();
 
+const PORT = 8000;
+
+app.use((req,res,next)=>{
+    console.log(`${req.method} ${req.url}`);
+    next();
+});
+
+app.get("/",(req,res)=>{
+    res.send("Student Management API is running");
+});
+
+app.listen(PORT, () => {
+    console.log(`Server started on port ${PORT}`);
+});
