2c35fc19bf64ca449df7569a9389a5b39537b52f
Aryan107rn <aryannakade123@gmail.com>
2026-09-13 20:41:35 +0530

    Added student.json data

--- student-api/server.js
+++ student-api/server.js
@@ -4,6 +4,8 @@ const app = express();
 
 const PORT = 8000;
 
+app.use(express.json());
+
 app.use((req,res,next)=>{
     console.log(`${req.method} ${req.url}`);
     next();
@@ -11,6 +13,11 @@ app.use((req,res,next)=>{
 
 app.get("/",(req,res)=>{
     res.send("Student Management API is running");
+});
+
+app.post("/api/students",(req,res)=>{
+    console(req.body);
+    res.json(req.body);
 });
 
 app.listen(PORT, () => {
